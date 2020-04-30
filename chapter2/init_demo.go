02378446593d589153876edbcee5d79b879fa14e
lovecrazy <630140621@qq.com>
2020-04-30 16:04:20 +0800

    使用匿名函数初始化

--- chapter2/init_demo.go
+++ chapter2/init_demo.go
@@ -7,12 +7,12 @@ var row = 5
 var table [][]int
 
 // 初始化一个 5行5列的 table
-func init()  {
+func init() {
 	fmt.Println("first init")
 	for i := 0; i < column; i++ {
 		table = append(table, []int{})
 		for j := 0; j < row; j++ {
-			table[i] = append(table[i], i * j)
+			table[i] = append(table[i], i*j)
 		}
 	}
 }
@@ -21,6 +21,18 @@ func init() {
 	fmt.Println("second init")
 }
 
-func printTable()  {
+var table2 [][]int = func() (table [][]int) {
+	fmt.Println("init table2 with anonymous func")
+	for i := 0; i < column; i++ {
+		table = append(table, []int{})
+		for j := 0; j < row; j++ {
+			table[i] = append(table[i], i*j)
+		}
+	}
+	return
+}()
+
+func printTable() {
 	fmt.Println(table)
+	fmt.Println(table2)
 }
